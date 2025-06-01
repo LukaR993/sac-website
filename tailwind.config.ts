fe041f9d29c887011c40e1ff89382fb61a5e9a65
Stefan Vukmanovic <87544626+Starko00@users.noreply.github.com>
2025-06-01 21:07:34 +0200

    container width

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,24 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1536px',
+        },
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
